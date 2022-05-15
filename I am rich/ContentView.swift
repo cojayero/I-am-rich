293c744a2af70bb98fa4dffa6780914a07f230bb
Baldomero Fernandez <f_baldomero@hotmail.com>
2022-05-15 22:04:24 +0200

    Codificación primera APP en swift ui - I am rich

--- I am rich/ContentView.swift
+++ I am rich/ContentView.swift
@@ -9,13 +9,31 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack{
+            Color(.systemTeal)
+            VStack {
+                Text("I am rich")
+                    .font(.system(size: 40
+                                 ))
+                    .fontWeight(.bold)
+                    .foregroundColor(Color.white)
+                    .padding()
+                Image("diamond")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 200,
+                           height: 200,
+                           alignment: .center)
+            }
+            
+            
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            .previewInterfaceOrientation(.portrait)
     }
 }
